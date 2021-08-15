2c3a0b44430a65546d48a5032ad89604aed2ea24
Liam Flaherty <liamjfla@gmail.com>
2021-08-15 22:12:05 +1000

    sine wave generation

--- Application/vehicleProcesses/sigGen/siggen.c
+++ Application/vehicleProcesses/sigGen/siggen.c
@@ -29,10 +29,18 @@ static StackType_t taskStack[SG_STACK_SIZE];
 // Task data
 static TaskHandle_t taskHandle;
 
+static uint32_t duty_cycle = 0;
+static uint64_t counter = 0;
+static float frequency = 100.0f; // Hz
+static const float TWO_PI = (float)M_2_PI;
+static float TWO_PI_F;
+
 // ------------------- Private methods -------------------
 static void SigGen_TaskMain(void* pvParameters)
 {
   logPrintS(logging, "SigGen_TaskMain begin\n", LOGGING_DEFAULT_BUFF_LEN);
+
+  TWO_PI_F = TWO_PI * frequency;
 
   while (1) {
     // Wait for notification to wake up
@@ -40,7 +48,24 @@ static void SigGen_TaskMain(void* pvParameters)
     if (notifiedValue > 0) {
       // ready to process
 
-      HAL_GPIO_TogglePin(GPIO1_GPIO_Port, GPIO1_Pin);
+      // Toggle debug indicator pin
+      HAL_GPIO_TogglePin(DEBUG_OUT_GPIO_Port, DEBUG_OUT_Pin);
+
+      // Hard set PWM to 50% for now
+//      duty_cycle++;
+//      if (duty_cycle > 19999) {
+//        duty_cycle = 0;
+//      }
+//      TIM8->CCR3 = 19999/5; // duty_cycle;
+
+      counter++;
+
+      float tSec = counter * 0.0001f;
+      float theta = TWO_PI_F * tSec;
+      float sine = 0.5f * sin(theta) + 0.5f; // produce sine value and offset (so it is bounded [0,1])
+      duty_cycle = 19999 * sine;  // scale to duty cycle values
+
+      TIM8->CCR3 = duty_cycle;
     }
 
   }
@@ -53,8 +78,11 @@ SigGen_Status_T SigGen_Init(
   logging = logger;
   logPrintS(logging, "SigGen_Init begin\n", LOGGING_DEFAULT_BUFF_LEN);
 
-  // Write direction
+  // Write direction (because of the weird prototype chip that's attached to PC7
   HAL_GPIO_WritePin(GPIO1_DIR_GPIO_Port, GPIO1_DIR_Pin, GPIO_PIN_RESET);  // data flows B->A (output from ECU)
+
+  // Start the PWM
+  HAL_TIM_PWM_Start(Mapping_GetTimerPhA(), MAPPING_TIM_CHANNEL_PHA_HIGH);
 
   // create main task
   taskHandle = xTaskCreateStatic(
